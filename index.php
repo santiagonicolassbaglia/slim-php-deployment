3aa0ccd5d75d4e2f7ad899178f11e395953ae150
santiagonicolassbaglia <santiagosbaglia@hotmail.com>
2023-06-21 21:41:36 -0300

    restaurant

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
 <?php
-// Error Handling
 error_reporting(-1);
 ini_set('display_errors', 1);
-
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface;
 use Slim\Factory\AppFactory;
 use Slim\Routing\RouteCollectorProxy;
+use Slim\Routing\RouteContext;
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+require_once './db/AccesoDatos.php';
+ 
+
+require_once './controllers/EmpleadosController.php';
+require_once './controllers/UsuariosController.php';
+
+// Load ENV
+//$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
 
 // Instantiate App
 $app = AppFactory::create();
@@ -16,26 +26,11 @@ $app = AppFactory::create();
 // Add error middleware
 $app->addErrorMiddleware(true, true, true);
 
-// Add parse body
-$app->addBodyParsingMiddleware();
+
+
+
 
 // Routes
-$app->get('[/]', function (Request $request, Response $response) {    
-    $payload = json_encode(array('method' => 'GET', 'msg' => "Bienvenido a SlimFramework 2023"));
-    $response->getBody()->write($payload);
-    return $response->withHeader('Content-Type', 'application/json');
-});
-
-$app->post('[/]', function (Request $request, Response $response) {    
-    $payload = json_encode(array('method' => 'POST', 'msg' => "Bienvenido a SlimFramework 2023"));
-    $response->getBody()->write($payload);
-    return $response->withHeader('Content-Type', 'application/json');
-});
-
-$app->post('/test', function (Request $request, Response $response) {    
-    $payload = json_encode(array('method' => 'POST', 'msg' => "Bienvenido a SlimFramework 2023"));
-    $response->getBody()->write($payload);
-    return $response->withHeader('Content-Type', 'application/json');
-});
-
+$app->post("/usuario", \UsuarioController::class. ":CargarUno");
+$app->post("/empleados", \EmpleadosController::class. ":CargarUno");
 $app->run();
