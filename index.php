86cd319f71e16262c4ab0f0e651598db966db52e
santiagonicolassbaglia <santiagosbaglia@hotmail.com>
2023-07-04 01:04:08 -0300

    TP2

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ use Slim\Routing\RouteContext;
 
 
 require_once './db/AccesoDatos.php';
-require __DIR__ . "./vendor/autoload.php";
+require __DIR__ . "/vendor/autoload.php";
 require_once './middlewares/AutentificadorJWT.php';
 
 require_once './controllers/LoguerController.php';
@@ -22,7 +22,7 @@ require_once './controllers/ProductosController.php';
 require_once './controllers/MesasController.php';
 require_once './controllers/PedidoController.php';
 require_once './controllers/FacturaController.php';
-
+require_once './controllers/csvControler.php';
 require_once './controllers/EncuestaController.php';
 require_once './middlewares/ConToken.php';
 require_once './middlewares/SoloAdmin.php';
@@ -60,7 +60,7 @@ $app->addErrorMiddleware(true, true, true);
  
 
 $app->group('/empleados', function (RouteCollectorProxy $group) {
-  $group->post('/altaEmpleado', \EmpleadosController::class . ':CargarUno');
+  $group->post('[/]', \EmpleadosController::class . ':CargarUno');
   // ->add(new SoloAdmin());
   $group->put('/{id}', \EmpleadosController::class . ':ModificarUno');//falta
   // ->add(new SoloAdmin())
@@ -71,6 +71,7 @@ $app->group('/empleados', function (RouteCollectorProxy $group) {
 });
 // ->add(new ConToken());
 
+             
 $app->group('/productos', function (RouteCollectorProxy $group) {
 $group->post('[/]', \ProductosController::class . ':CargarProducto');
 $group->get('/csv', \ProductosController::class . ':ExportarProductos');
@@ -91,27 +92,66 @@ $group->put('/cambiarEstado', \MesasController::class . ':CambiarEstadoMesa');//
 // ->add(new SoloAdmin());
 $group->delete('/cerrarMesa', \MesasController::class . ':CerrarMesa');
 // ->add(new SoloAdmin());
-});
-// ->add(new ConToken());
+$group->put('/comiendo/{idMesa}', \MesasController::class . ':CambiarEstadoMesa');
+$group->put('/llevarCuenta/{idMesa}', \MesasController::class . ':cobrar')
+  ->add(\Validaciones::validarRoles(['Socio','Mozo']));
+
+  $group->put('/cerrarMesa/{idMesa}', \MesasController::class . ':CerrarMesa')
+  ->add(\Validaciones::validarRoles(['Socio']));
+  
+})->add(\Validaciones::validarRoles(['Socio', 'Mozo']))
+->add(\Validaciones::class . ':validarJWTUsuario');
 
 $app->group('/pedidos', function (RouteCollectorProxy $group) { 
-$group->post('[/]', \PedidoController::class . ':CargarPedido')->add(\Validaciones::class . ':ValidarSocio') ;
-$group->get('/listarPedidos', \PedidoController::class . ':MostrarPedidos')->add(\Validaciones::class . ':ValidarJWT') ;
-$group->get('/MostrarPedidosEmpleado', \PedidoController::class . ':MostrarPedidosEmpleado');
-$group->get('/ConsultarPedidosListos', \PedidoController::class . ':ConsultarPedidosListos')->add(\Validaciones::class . ':ValidarJWT') ;
-$group->get('/MostrarPedidosPreparados', \PedidoController::class . ':MostrarPedidosPreparados');
-$group->get('/MesaPopular',  \PedidoController::class . ':ConsultarMesaPopular')->add(\Validaciones::class . ':ValidarJWT') ;
-$group->put('/prepararPedido', \PedidoController::class . ':PrepararPedido');
-$group->put('/PedidoListo', \PedidoController::class . ':CambiarEstadoListo');
-})->add(new ConToken());
+$group->post('[/]', \PedidoController::class . ':CargarPedido')
+->add(\Validaciones::validarRoles(['Socio', 'Mozo']))->add(\Validaciones::class . ':validarJWTUsuario');
+ 
+$group->get('/listarPedidos', \PedidoController::class . ':MostrarPedidos')
+->add(\Validaciones::validarRoles(['Socio']))->add(\Validaciones::class . ':validarJWTUsuario');
+ 
+$group->get('/listarPedidosPendientes', \PedidoController::class . ':MostrarPedidosPendientes')
+->add(\Validaciones::class . ':validarJWTUsuario');
 
-$app->post('/Encuesta', \EncuestaController::class . ':CargarEncuesta');
+$group->post('/prepararPedido', \PedidoController::class . ':PrepararPedido')
+->add(\Validaciones::class . ':validarJWTUsuario');
+ 
+
+$group->get('/{idMesa}/{codigoMesa}', \PedidoController::class . ':mostrarMesacodigo');
+
+$group->get('/MostrarPedidosPreparacion', \PedidoController::class . ':MostrarPedidosPreparacion')
+->add(\Validaciones::class . ':validarJWTUsuario');
+
+$group->post('/paraServir', \PedidoController::class . ':paraServir')->add(\Validaciones::class . ':validarJWTUsuario');
+$group->get('/MostrarPedidosParaServir', \PedidoController::class . ':MostrarPedidosParaServir')
+->add(\Validaciones::validarRoles(['Socio', 'Mozo']))->add(\Validaciones::class . ':validarJWTUsuario');
+
+$group->post('/servir', \PedidoController::class . ':servir')->add(\Validaciones::validarRoles(['Socio', 'Mozo']))->add(\Validaciones::class . ':validarJWTUsuario');
+
+
+
+
+
+
+
+
+$group->get('/MostrarPedidosEmpleado', \PedidoController::class . ':MostrarPedidosEmpleado');
+$group->get('/ConsultarPedidosListos', \PedidoController::class . ':ConsultarPedidosListos');//->add(\Validaciones::class . ':ValidarJWT') ;
+$group->get('/MostrarPedidosPreparados', \PedidoController::class . ':MostrarPedidosPreparados');
+$group->get('/MesaPopular',  \PedidoController::class . ':ConsultarMesaPopular');//->add(\Validaciones::class . ':ValidarJWT') ;
+$group->put('/PedidoListo', \PedidoController::class . ':CambiarEstadoListo');
+}) ;
+ 
+
+$app->post('/Encuesta', \EncuestaController::class . ':CargarUno');
 $app->post('/Facturar', \FacturaController::class . ':CargarFactura')->add(\Validaciones::class . ':ValidarJWT') ;
 $app->get('/MostrarFacturas', \FacturaController::class . ':MostrarFacturas');
-$app->get('/MejoresEncuestas', \EncuestaController::class . ':MostrarMejores')->add(\Validaciones::class . ':ValidarJWT') ;
+ 
+$app->get('/MejoresEncuestas', \EncuestaController::class . ':MejoresEncuestas')->add(\Validaciones::validarRoles(['Socio']))->add(\Validaciones::class . ':validarJWTUsuario') ;
 $app->post('/demoraPedido', \PedidoController::class . ':ConsultarDemoraPedido');
 $app->post('/loguin', \LoguerController::class . ':GenerarToken');
 
+$app->post('/csv/cargar/{baseDatos}', \csvControler::class . ':cargarcsv');
+$app->get('/archivos/descargar/{baseDatos}',  \csvControler::class . ':descargarcsv');
 $app->get('[/]', function (Request $request, Response $response) {    
   $response->getBody()->write("TP Programacion III");
   return $response;
